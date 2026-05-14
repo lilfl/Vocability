2d3a66d6f22e66472a16043122afa5f4fa3e62a0
Itaru Fukagawa <if.tdg555w@gmail.com>
2026-05-14 16:56:08 +0900

    feat: preserve add vocabulary state when navigating to settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import { SettingsProvider } from './lib/SettingsContext'
+import { AddPageProvider } from './lib/AddPageContext'
 import Navbar from './components/layout/Navbar'
 import HomePage from './pages/HomePage'
 import QuizPage from './pages/QuizPage'
@@ -9,13 +10,15 @@ import SettingsPage from './pages/SettingsPage'
 export default function App() {
   return (
     <SettingsProvider>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/quiz" element={<QuizPage />} />
-        <Route path="/add" element={<AddPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
-      </Routes>
+      <AddPageProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/quiz" element={<QuizPage />} />
+          <Route path="/add" element={<AddPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+        </Routes>
+      </AddPageProvider>
     </SettingsProvider>
   )
 }
